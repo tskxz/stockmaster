99d41ff0d14765477bf228730720d929a74cb00b
Luis Martins <luiss_martinss_21@hotmail.com>
2025-01-25 09:59:16 +0000

    Inicial Commit Front-End

--- client/src/Screens/HomeScreen.jsx
+++ client/src/Screens/HomeScreen.jsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/screens/homescreen.css";
+import homeimg from "../assets/homeimg.png";
 
 const HomeScreen = () => {
-  return (
-    <>
-        <h1>Bem vindo ao StockMaster</h1>
-        <ul>
-          <li> <Link to={"/meus_armazens"}>Veja os seus armazéns </Link></li>
-        </ul>
-      </>
-  )
-}
+        return (
+                <div className="app">
+                        <div className="app-leftside">
+                                <h1>Stock</h1>
+                                <h1>Master</h1>
 
-export default HomeScreen
+                                <div className="app-description">
+                                        <p>
+                                                A sua solução para gestão de
+                                                stock empresarial.
+                                        </p>
+
+                                        <p>
+                                                📦 Controle Total do Stock
+                                                Organize, monitore e otimize os
+                                                níveis de stock da sua empresa
+                                                com facilidade.
+                                        </p>
+                                        <p>
+                                                📊 Relatórios e Insights Tome
+                                                decisões estratégicas com base
+                                                em dados precisos e relatórios
+                                                detalhados.
+                                        </p>
+                                        <p>
+                                                ⚙️ Eficiência e Agilidade Reduza
+                                                erros e melhore a produtividade
+                                                com ferramentas intuitivas e
+                                                automatizadas.
+                                        </p>
+                                        <p>
+                                                Comece agora e leve a gestão da
+                                                sua empresa para o próximo
+                                                nível!
+                                        </p>
+                                </div>
+                                <ul>
+                                        <li>
+                                                {" "}
+                                                <Link
+                                                        className="button"
+                                                        to={"/meus_armazens"}
+                                                >
+                                                        Veja os seus armazéns{" "}
+                                                </Link>
+                                        </li>
+                                </ul>
+                        </div>
+
+                        <div className="app-rightside">
+                                <img src={homeimg} alt="homeimg"></img>
+                        </div>
+                </div>
+        );
+};
+
+export default HomeScreen;
